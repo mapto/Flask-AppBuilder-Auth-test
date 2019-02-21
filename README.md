e1db14812f682e02143fdfd21da99d6a40f12ceb
Martin Ruskov <martin@unriddle.it>
2019-02-21 11:20:24 +0100

    command line parametrisation

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 Flask-AppBuilder LDAP configuration test
 ----------------------------------------
 
-A simple app that allows ([not yet](https://github.com/mapto/Flask-AppBuilder-LDAP-test/blob/master/config.py)) login via LDAP.
-
-This is used to debug an installation of [Apache Superset](https://superset.incubator.apache.org).
+A simple app that allows ([not yet](https://github.com/mapto/Flask-AppBuilder-LDAP-test/blob/master/config.py)) login via LDAP. This is used to debug an installation of [Apache Superset](https://superset.incubator.apache.org).
 
 To allow testing in a quasi-production environment, prepackaged in a binary.
+
+# Dependencies
+
+Requires python3
+
+    pip install flask-appbuilder python-ldap pyinstaller docopt
--- fab-ldap-test.py
+++ fab-ldap-test.py
@@ -2,9 +2,30 @@
 """FlaskAppBuilder-LDAP Test
 
 Usage:
-	
+	fab-ldap-test --help
+	fab-ldap-test [options]
+
+Examples:
+	fab-ldap-test --port=8080
+	fab-ldap-test --type=DB
+	fab-ldap-test --port=8080 --type=DB
+
+Options:
+ -h --help         Show this screen.
+ -p --port=<port>  Server port [default: 8080].
+ -d --debug        Start in debug mode.
+ -t --type=<type>  Authentication type [default: DB].
 """
+from docopt import docopt
 from app import app
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+from flask_appbuilder.security.manager import AUTH_DB, AUTH_LDAP
+from config import AUTH_TYPE
+
+auth_types = {"DB": AUTH_DB, "LDAP": AUTH_LDAP}
+
+args = docopt(__doc__)
+if args['--type']:
+	AUTH_TYPE = auth_types[args['--type']]
+app.run(host='0.0.0.0', port=args['--port'], debug=args['--debug'])
 
