b1da5e6d342e5e8c1362bab0b3450c83d0ca6f5d
Martin Ruskov <martin@unriddle.it>
2019-02-21 14:37:15 +0100

    reading LDAP properties from an external file

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-Flask-AppBuilder LDAP configuration test
-----------------------------------------
+# Flask-AppBuilder LDAP configuration test
 
-A simple app that allows ([not yet](https://github.com/mapto/Flask-AppBuilder-LDAP-test/blob/master/config.py)) login via LDAP. This is used to debug an installation of [Apache Superset](https://superset.incubator.apache.org).
+A simple tool that allows login via LDAP. This is used to debug an installation of [Apache Superset](https://superset.incubator.apache.org), allowing for testing in a quasi-production environment. The tool is prepackaged in a binary and takes the [LDAP properties](https://flask-appbuilder.readthedocs.io/en/latest/security.html#authentication-ldap) from an external file via the the `--config` option. If the option is not specified defaults to an `admin:admin` user.
 
-To allow testing in a quasi-production environment, prepackaged in a binary.
+For usage options see documentation in [fab-ldap-test.py](https://github.com/mapto/Flask-AppBuilder-LDAP-test/blob/master/fab-ldap-test.py) or use the `--help` option
 
-# Dependencies
+## Dependencies
 
-Requires python3
+Requires [python3](https://www.python.org/downloads/). To install further prerequisites, use:
 
     pip install flask-appbuilder python-ldap pyinstaller docopt
--- fab-ldap-test.py
+++ fab-ldap-test.py
@@ -7,25 +7,34 @@ Usage:
 
 Examples:
 	fab-ldap-test --port=8080
-	fab-ldap-test --type=DB
-	fab-ldap-test --port=8080 --type=DB
+	fab-ldap-test --config=custom_config.py -d
 
 Options:
- -h --help         Show this screen.
- -p --port=<port>  Server port [default: 8080].
- -d --debug        Start in debug mode.
- -t --type=<type>  Authentication type [default: DB].
+ -h --help             Show this screen.
+ -p --port=<port>      Port used by this test server [default: 8080].
+ -d --debug            Start in debug mode.
+ -c --config=<config>  LDAP config file, with variables as in https://flask-appbuilder.readthedocs.io/en/latest/security.html#authentication-ldap 
+
+For further documentation see README file.
+
 """
+from importlib import import_module
+
 from docopt import docopt
 from app import app
 
 from flask_appbuilder.security.manager import AUTH_DB, AUTH_LDAP
-from config import AUTH_TYPE
-
-auth_types = {"DB": AUTH_DB, "LDAP": AUTH_LDAP}
+import config
 
 args = docopt(__doc__)
-if args['--type']:
-	AUTH_TYPE = auth_types[args['--type']]
+
+AUTH_TYPE = AUTH_LDAP if args['--config'] else AUTH_DB	
+
+if args['--config']:
+	custom_config = import_module(args['--config'].split('.')[0])  # Get rid of file extension. Providing modules as filename is easier with autocomplete
+	vals = [v for v in dir(custom_config) if v.startswith("AUTH_LDAP_")]
+	for v in vals:
+		setattr(config, v, getattr(custom_config, v))
+
 app.run(host='0.0.0.0', port=args['--port'], debug=args['--debug'])
 
